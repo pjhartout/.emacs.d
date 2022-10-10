d889d25e641c7291d50c7e63a38cfbe9d2da5939
Philip Hartout <philip.hartout@protonmail.com>
2022-10-10 11:01:39 +0200

    set explicit path to wakatime cli

--- projects/projects-time.el
+++ projects/projects-time.el
@@ -1,3 +1,4 @@
+
 ;; Packages related to tracking time.
 (require 'package)
 (setq package-list
@@ -8,7 +9,10 @@
   (unless (package-installed-p package)
     (package-install package)))
 
+
+
 (global-wakatime-mode)
+(setq wakatime-cli-path "/Users/philiphartout/.wakatime/wakatime-cli")
 
 (provide 'projects-time)
 
