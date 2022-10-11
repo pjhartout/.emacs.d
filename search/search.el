976686688d50807e061d93a0a4dd80f5c899b55a
Philip Hartout <philip.hartout@protonmail.com>
2022-10-11 11:37:20 +0200

    better completion with company mode

--- search/search.el
+++ search/search.el
@@ -11,4 +11,20 @@
 (global-set-key "\C-s" 'swiper)
 (global-set-key (kbd "M-x") 'counsel-M-x)
 
+;; Company mode for completion
+
+(use-package company
+  :after lsp-mode
+  :hook (lsp-mode . company-mode)
+  :bind (:map company-active-map
+         ("<tab>" . company-complete-selection))
+        (:map lsp-mode-map
+         ("<tab>" . company-indent-or-complete-common))
+  :custom
+  (company-minimum-prefix-length 1)
+  (company-idle-delay 0.0))
+
+(use-package company-box
+  :hook (company-mode . company-box-mode))
+
 (provide 'search)
