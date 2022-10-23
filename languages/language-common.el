f3d7eb61f75947b2cb841f75f6942e50c90af34f
Philip Hartout <philip.hartout@protonmail.com>
2022-10-24 00:45:51 +0200

    comment lsp for all languages to avoid overlapping errors with language-specific lsp

--- languages/language-common.el
+++ languages/language-common.el
@@ -9,19 +9,19 @@
     (package-install package)))
 
 
-;; LSP Mode, used for multiple languages
-(use-package lsp-mode
-  :init
-  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
-  (setq lsp-keymap-prefix "C-c l")
-  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
-         (python-mode . lsp)
-         ;; if you want which-key integration
-         (lsp-mode . lsp-enable-which-key-integration))
-  :commands lsp)
+;; ;; LSP Mode, used for multiple languages
+;; (use-package lsp-mode
+;;   :init
+;;   ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+;;   (setq lsp-keymap-prefix "C-c l")
+;;   :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+;;          (python-mode . lsp)
+;;          ;; if you want which-key integration
+;;          (lsp-mode . lsp-enable-which-key-integration))
+;;   :commands lsp)
 
-;; optionally
-(use-package lsp-ui :commands lsp-ui-mode)
+;; ;; optionally
+;; (use-package lsp-ui :commands lsp-ui-mode)
 
 ;; Which key
 (use-package which-key
