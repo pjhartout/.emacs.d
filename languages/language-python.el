33ca78573979908d31b9fab86b8e74f191dd5b88
Philip Hartout <philip.hartout@protonmail.com>
2022-10-24 00:44:40 +0200

    add blacken + remove lsp to avoid lsp-related errors

--- languages/language-python.el
+++ languages/language-python.el
@@ -10,21 +10,26 @@
 ;; Main python mode
 (require 'python)
 
-;; Language Server Protocol
-(use-package lsp-pyright
-  :ensure t
-  :hook (python-mode . (lambda ()
-                         (require 'lsp-pyright)
-                         (lsp))))  ; or lsp-deferred
+;; ;; Language Server Protocol
+;; (use-package lsp-pyright
+;;   :ensure t
+;;   :hook (python-mode . (lambda ()
+;;                          (require 'lsp-pyright)
+;;                          (lsp))))  ; or lsp-deferred
 
-;; Format code on save using black
-(use-package python-black
-  :demand t
-  :after python
-  :hook (python-mode . python-black-on-save-mode-enable-dwim))
+;; format code on save using blacken
+;; use-package does not seem to work here.
 
+(require 'blacken)
+(setq blacken-line-length 79)
+(setq blacken-executable "/home/paul/.virtualenvs/py3.8/bin/black")
+(add-hook 'python-mode-hook 'blacken-mode 'too-long-lines-mode)
+
+;; py-isort
 (require 'py-isort)
 (add-hook 'before-save-hook 'py-isort-before-save)
 
 (use-package poetry
   :ensure t)
+
+(setq python-indent-guess-indent-offset 4)
