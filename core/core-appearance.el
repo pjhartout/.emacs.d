4f0b63b6b58dab4ef18ee8828e77cd908b606f86
Philip Hartout <philip.hartout@protonmail.com>
2022-10-24 01:36:34 +0200

    add better help contexts

--- core/core-appearance.el
+++ core/core-appearance.el
@@ -79,5 +79,18 @@
   (invert-face 'mode-line)
   (run-with-timer 0.1 nil #'invert-face 'mode-line))
 
+;; Helpful help contexts
+
+(use-package helpful
+  :commands (helpful-callable helpful-variable helpful-command helpful-key)
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
 (provide 'core-appearance)
 
