3974bcd9b54b8366cdbe8797f6f0de2a01858f33
Philip Hartout <philip.hartout@protonmail.com>
2022-10-24 16:06:18 +0200

    additional selected packages

--- init.el
+++ init.el
@@ -50,7 +50,7 @@
  '(custom-safe-themes
    '("5f128efd37c6a87cd4ad8e8b7f2afaba425425524a68133ac0efd87291d05874" default))
  '(package-selected-packages
-   '(doom-themes helpful beacon undo-tree restart-emacs projectile nord-theme magit counsel)))
+   '(blacken lsp-pyright smartparens-config doom-themes helpful beacon undo-tree restart-emacs projectile nord-theme magit counsel)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
