ad00944b669c368676a81fda08acc768e43ced87
Philip Hartout <philip.hartout@protonmail.com>
2022-10-24 16:04:57 +0200

    add general lsp mode, shift line numbers to editor, smartparens mode

--- languages/language-common.el
+++ languages/language-common.el
@@ -9,21 +9,24 @@
     (package-install package)))
 
 
-;; ;; LSP Mode, used for multiple languages
-;; (use-package lsp-mode
-;;   :init
-;;   ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
-;;   (setq lsp-keymap-prefix "C-c l")
-;;   :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
-;;          (python-mode . lsp)
-;;          ;; if you want which-key integration
-;;          (lsp-mode . lsp-enable-which-key-integration))
-;;   :commands lsp)
+;; LSP Mode, used for multiple languages
+(use-package lsp-mode
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (python-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
 
-;; ;; optionally
-;; (use-package lsp-ui :commands lsp-ui-mode)
+(use-package lsp-ui
+  :ensure t
+  :after lsp-mode
+  :hook
+  (lsp-mode-hook . lsp-ui-mode))
 
-;; Which key
+;; which key
 (use-package which-key
     :config
     (which-key-mode)
@@ -40,10 +43,10 @@
 (define-key yas-minor-mode-map [(tab)] nil)
 (define-key yas-minor-mode-map (kbd "TAB") nil)
 
-;; For any programming language, we enable line numbers
-(add-hook 'prog-mode-hook 'display-line-numbers-mode)
-;; and also for org mode
-(add-hook 'org-mode-hook 'display-line-numbers-mode)
+;; Parenthesis behaviour
+(use-package smartparens)
+(require 'smartparens-config)
+(add-hook 'prog-mode-hook #'smartparens-mode)
 
 
 (provide 'language-common)
