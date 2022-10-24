94080398bc8daf6719ed0b7b914f428d32328ba7
Philip Hartout <philip.hartout@protonmail.com>
2022-10-25 01:26:19 +0200

    better use of use-package and proper IDE config for python!

--- languages/language-python.el
+++ languages/language-python.el
@@ -1,16 +1,7 @@
-(require 'package)
-(setq package-list
-      '(python-black py-isort poetry))
-
-; install the packages
-(dolist (package package-list)
-  (unless (package-installed-p package)
-    (package-install package)))
-
 ;; Main python mode
+
 ;; Python
 (use-package python
-  :ensure t
   :custom
   ;; (python-shell-completion-native-enable nil)
   (python-indent-guess-indent-offset-verbose nil)
@@ -25,54 +16,44 @@
 
 ;; Language Server Protocol
 (use-package lsp-pyright
-  :ensure t
   :after (python lsp-mode)
-  :custom
-  (lsp-pyright-venv-path (getenv "WORKON_HOME"))
+  ;; :custom
+  ;; (lsp-pyright-venv-path (getenv "WORKON_HOME"))
   :hook
   (python-mode-hook . (lambda ()
 			(require 'lsp-pyright) (lsp))))
 
-
 ;; pyenv-mode
 ;; Integrate pyenv with python-mode
 (use-package pyenv-mode
-  :ensure t
   :if (eq system-type 'darwin)
   :after python
   :hook
   (python-mode-hook . pyenv-mode))
 
 ;; Poetry virtual environment support
-(use-package poetry
-  :ensure t)
-
+(use-package poetry)
+(add-hook 'python-mode-hook 'poetry-tracking-mode)
+(setq poetry-tracking-strategy 'projectile)
+(setq poetry-tracking-strategy 'switch-buffer)
 
 ;; format code on save using blacken
 ;; use-package does not seem to work here.
-(use-package blacken)
+(use-package blacken
+  :bind (:map python-mode-map ("C-c C-b" . blacken-buffer)))
+
 (setq blacken-line-length 79)
-(setq blacken-executable "/home/paul/.virtualenvs/py3.8/bin/black")
+(setq blacken-executable "/Users/philiphartout/.pyenv/shims/black")
 (add-hook 'python-mode-hook 'blacken-mode 'too-long-lines-mode)
 
-;; Sort imports with py-isort
-(require 'py-isort)
+(define-advice org-edit-src-exit (:before (&rest _args) format-python)
+  "Run `blacken-buffer' when leaving an org-mode Python source block."
+  (when (eq major-mode 'python-mode)
+    (blacken-buffer)))
+
+;; sort imports with py-isort
+(use-package py-isort)
 (add-hook 'before-save-hook 'py-isort-before-save)
 
-;; ;; pyvenv
-;; ;; Python virtual environment support
-;; (use-package pyvenv
-;;   :ensure t
-;;   :after python
-;;   :custom
-;;   (pyvenv-workon "env")
-;;   :hook
-;;   ((python-mode-hook . pyvenv-mode)
-;;    (pyvenv-post-activate-hooks . pyvenv-restart-python)
-;;    (pyvenv-post-deactivate-hooks . pyvenv-restart-python))
-;;   :config
-;;   (defalias 'workon 'pyvenv-workon)
-;;   (defalias 'deactivate 'pyvenv-deactivate))
-
-
 (setq python-indent-guess-indent-offset 4)
+(provide 'language-python)
