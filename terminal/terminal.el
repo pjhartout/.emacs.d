f55cf26bd71872ec14cfa87a864214e424fdccfc
Philip Hartout <philip.hartout@protonmail.com>
2022-12-05 09:22:54 +0100

    add eshell config

--- terminal/terminal.el
+++ terminal/terminal.el
@@ -1,5 +1,25 @@
-(setq eshell-prompt-function (lambda () "A simple prompt." "λ ")
-      eshell-prompt-regexp    "^λ ")
+(use-package load-bash-alias
+  :ensure t
+  :defer t
+  :config
+  (setq load-bash-alias-bashrc-file "~/.aliases"))
+
+(use-package esh-autosuggest
+  :ensure t
+  :defer t
+  :hook
+  (eshell-mode . esh-autosuggest-mode))
+
+(use-package eshell
+  :config
+  ;; Eshell prompt customization
+  (setq eshell-highlight-prompt t)
+  (setq eshell-prompt-regexp "^[^# λ \n]*[# λ ]"
+      eshell-prompt-function (lambda nil
+        (concat
+	 (if (string= (eshell/pwd) (getenv "HOME"))
+	     "~" (eshell/pwd)) 
+	 (if (= (user-uid) 0) "# " " λ ")))))
 
 ;; All kinds of useful terminal related things
 ;; see https://github.com/daviwil/emacs-from-scratch/blob/bbfbc77b3afab0c14149e07d0ab08d275d4ba575/Emacs.org#terminals
